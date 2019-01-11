21c8d205f67fcbb41cea6879afc79d335399fd7d
Han Du <han.du@dfki.de>
2019-01-11 23:36:42 +0100

    add test case for pyautogui

--- venv/Include/experiments/selenium/test.py
+++ venv/Include/experiments/selenium/test.py
@@ -11,8 +11,8 @@ import pyautogui
 
 
 def skip_ads():
-    driver = webdriver.Chrome(executable_path=r'E:\tmp\chromedriver_win32\chromedriver.exe')
-    url = "https://www.youtube.com/watch?v=ghr4MkSuxqw&start_radio=1&list=RDghr4MkSuxqw"
+    driver = webdriver.Chrome(executable_path=r'../../chromedriver_win32\chromedriver.exe')
+    url = "https://www.youtube.com/watch?time_continue=1&v=1KrMEsg7hr4"
     # url = "http://www.google.com"
     driver.get(url)
     # driver.manage().window().maximize()
@@ -20,10 +20,16 @@ def skip_ads():
     driver.maximize_window()
     # player_status = driver.execute_script("return document.getElementById('movie_player').getPlayerState()")
     # print(player_status)
-    movie_player = driver.find_element_by_id('movie_player')
-    print(movie_player)
+    # movie_player = driver.find_element_by_id('movie_player')
+    # print(movie_player)
+    ad_elem = driver.find_element_by_class_name('branding-img-container')
+    print(ad_elem)
+    # player_status = driver.execute_script("return document.getElementById('branding-img-container').getPlayerState()")
+    # print("play status: ", player_status)
+    ad_elem.click()
     time.sleep(5)
-    movie_player.click()
+    # movie_player.click()
+
     # youtube_search = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div[3]/form/div/input")
     # clickButton = driver.find_element_by_xpath("/html/body/div[2]/div[2]/div[2]/div/div[3]/form/button")
 
@@ -60,6 +66,7 @@ def test():
             # wait for the page to load
             # time.sleep(10)
             # make sure the browser is in focus
+            # browser.set_window_size(1048, 786)
             browser.maximize_window()
             time.sleep(10)
             # click the start test button
